5b80bc5d136c66858a340f94226ec0aa68c814dc
jreynoso <jreynoso@gmail.com>
2020-05-06 14:46:21 -0500

    update the frame tests

--- src/components/__tests__/Frame.test.js
+++ src/components/__tests__/Frame.test.js
@@ -14,16 +14,15 @@ test('formats standard gutterball frame correctly', () => {
   const { getByText, getAllByText } = render(<Frame {...frame}/>)
   const rolls = getAllByText(/-/i)
   expect(rolls).toHaveLength(2)
-  const score = getByText(testScore)
+  const score = getByText(`${testScore}`)
   expect(score).toBeInTheDocument()
 })
 
 test('formats standard spare frame correctly', () => {
-  const testScore = Faker.random.number()
   const frame = {
     number: 1,
     rolls: [3,7],
-    score: testScore,
+    score: Faker.random.number(),
     isCurrentFrame: true
   }
   const { getByText } = render(<Frame {...frame}/>)
@@ -31,6 +30,48 @@ test('formats standard spare frame correctly', () => {
   expect(rollOne).toBeInTheDocument()
   const rollTwo = getByText('/')
   expect(rollTwo).toBeInTheDocument()
-  const score = getByText(testScore)
-  expect(score).toBeInTheDocument()
+})
+
+test('formats standard strike frame correctly', () => {
+  const frame = {
+    number: 1,
+    rolls: [10],
+    score: Faker.random.number(),
+    isCurrentFrame: true
+  }
+  const { getByText } = render(<Frame {...frame}/>)
+  const strike = getByText('X')
+  expect(strike).toBeInTheDocument()
+})
+
+test('formats final frame correctly', () => {
+  const frame = {
+    number: 10,
+    rolls: [10, 3, 7],
+    score: Faker.random.number(),
+    isCurrentFrame: true
+  }
+  const { getByText } = render(<Frame {...frame}/>)
+  const strike = getByText('X')
+  expect(strike).toBeInTheDocument()
+  const rollTwo = getByText('3')
+  expect(rollTwo).toBeInTheDocument()
+  const rollThree = getByText('/')
+  expect(rollThree).toBeInTheDocument()
+})
+
+test('formats final frame correctly when strike followed by gutterball and spare', () => {
+  const frame = {
+    number: 10,
+    rolls: [10, 0, 10],
+    score: Faker.random.number(),
+    isCurrentFrame: true
+  }
+  const { getByText } = render(<Frame {...frame}/>)
+  const strike = getByText('X')
+  expect(strike).toBeInTheDocument()
+  const rollTwo = getByText('-')
+  expect(rollTwo).toBeInTheDocument()
+  const rollThree = getByText('/')
+  expect(rollThree).toBeInTheDocument()
 })
